29f5f922448e81560a4a39c68e36f249d9925acf
andriiglukhyi <andrigluxii@gmail.com>
2018-01-22 19:02:54 -0800

    new

--- server.js
+++ server.js
@@ -3,8 +3,9 @@
 
 const express = require('express');
 const pg = require('pg');
-// const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended : true}));
 
 const PORT = process.env.PORT;
 
@@ -22,11 +23,10 @@ client.on('error', err => {
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true}));
 
 app.get('/', (req, res) => response.sendFile('index..html', {root: 'https://ryanandrii-booksapp.herokuapp.com'}));
 
-app.get('/db/person', function(request, response) {
+app.get('db/person', function(request, response) {
     client.query('SELECT * FROM persons;')
     .then(function(data) {
       response.send(data);
