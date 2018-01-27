ad9f80506249eebdc211f5bdba3bf758aba766b7
andriiglukhyi <andrigluxii@gmail.com>
2018-01-27 15:12:47 -0800

    new

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-'use strict';
+'use strict'
 
-const cors = require('cors');
+//These are our dependencies
 const express = require('express');
+const cors = require('cors');
 const pg = require('pg');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser').urlencoded({extended: true});
+
+//Setting up our applications
 const app = express();
 const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+const TOKEN = process.env.TOKEN;
 
-
-const connectionString = process.env.DATABASE_URL;
-const client = new pg.Client(connectionString);
+//Setting up our database
+const client = new pg.Client(process.env.DATABASE_URL);
+// const client = new pg.Client('postgres://localhost:5432/books_app');
 client.connect();
+client.on('error', err => console.error(err));
 
+//Instantiating middleware
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
+//Endpoints below here
 
 //Getting stuff from the database to render on pages
 app.get('/books', (req, res) => {
@@ -26,13 +32,13 @@ app.get('/books', (req, res) => {
 
 //Getting a single book
 app.get('/books/:id', (req, res) => {
-  client.query(`SELECT * FROM books WHERE id=${req.params.book_id}`)
+  client.query(`SELECT * FROM books WHERE book_id=${req.params.id}`)
     .then(results => res.send(results.rows))
     .catch(console.error);
 });
 
 //Adding a book to the database
-app.post('/books', bodyParser, (req, res) => {
+app.post('books', bodyParser, (req, res) => {
   let {title, author, isbn, url, description} = req.body;
   client.query(`
       INSERT INTO books(title, author, isbn, url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING`,
@@ -41,11 +47,13 @@ app.post('/books', bodyParser, (req, res) => {
     .then(res.sendStatus(201))
     .catch(console.error);
 });
+
+
 //Delete a book
 app.delete('/books/:id', (req, res) => {
   client.query(`
     DELETE FROM books
-    WHERE id=${req.params.id}`)
+    WHERE book_id=${req.params.book_id}`)
     .then(() => res.send(204))
     .catch(console.error);
 });
@@ -56,32 +64,14 @@ app.put('/books', bodyParser, (req, res) => {
   client.query(`
     UPDATE books
     SET title=$1, author=$2, isbn=$3, url=$4, description=$5
-    WHERE id=${req.body.book_id}`,
+    WHERE book_id=${req.body.book_id}`,
   [title, author, isbn, url, description]
   )
     .then(res.sendStatus(200))
     .catch(console.error)
 });
 
+//This is a redirect
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
-createTable();
-
-app.listen(PORT, () => {
-  console.log(`currently listening on ${PORT}`);
-});
-
-function createTable() {
-  client.query(`
-    CREATE TABLE IF NOT EXISTS books(
-      id SERIAL PRIMARY KEY,
-      title VARCHAR(256),
-      author VARCHAR(256),
-      isbn VARCHAR(256),
-      url VARCHAR(256),
-      description text NOT NULL
-    );`
-  )
-  .then(function(response) {
-    console.log('books table are done');
-  });
-};
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
