1d0ff325a7b5816023bbb87cb4e7d784a72fff85
andriiglukhyi <andrigluxii@gmail.com>
2018-01-27 17:59:09 -0800

    new

--- server.js
+++ server.js
@@ -1,36 +1,50 @@
-'use strict'
+'use strict';
 
-
-const express = require('express');
+//ALLOWS EVERYONE TO VIEW
 const cors = require('cors');
+// ALLOWS SERVER SIDE JS TO BE EXECUTED
+const express = require('express');
+// POSTGRES DBMS
 const pg = require('pg');
-const bodyParser = require('body-parser').urlencoded({extended: true});
-
-
+// ALLOWS FOR PARSEING OF INCOMPING API POSTS AND PUTS, MANIPULATES REQ BODY BEFORE IT HITS SERVER
+const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+//ALLOWS NODE TO INTERACT WITH LOCAL FILES CUS DB IS RUNNING LOCALLY
 
-const client = new pg.Client(process.env.DATABASE_URL);
-
+const conString = 'postgres://localhost:5432/books_app';
+// const conString = process.env.DATABASE_URL;
+const client = new pg.Client(conString);
+// HOW WE CONNECT TO OUR DB
 client.connect();
 
-
+// ***  MIDDLEWARE  ***
+// CROSS ORIGIN SCRIPTING
 app.use(cors());
+// TELLS BODYPARSER TO USE JSON WHEN POSTING AND PUTING DATA INTO DB
+app.use(bodyParser.json());
+// ALLOWS BODYPARSER TO PARSE NESTED OBJECTS
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send('hello');
+})
+// ROUTES 
 
-app.get('/books', function(req, res) {
-  client.query('SELECT * FROM books;')
-  .then(function(data) {
-    res.send(data.rows);
-  })
-  .catch(function(err) {
-    console.error(err);
+app.get('/v1/books', function(req, res) {
+    // console.log('app.get /v1/books');
+    client.query('SELECT * FROM books;')
+    .then(function(data) {
+      res.send(data.rows);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
   });
-});
 
-
-app.get('/books/:id', function (req,res) {
-  client.query(`SELECT * FROM books WHERE id = ${req.params.id};`)
+app.get('/v1/books/:book_id', function (req,res) {
+  // console.log(req);
+  client.query(`SELECT * FROM books WHERE book_id = ${req.params.book_id};`)
   .then(function(data){
     res.send(data.rows);
   })
@@ -39,59 +53,60 @@ app.get('/books/:id', function (req,res) {
   });
 });
 
-
-//Adding a book to the database
-app.post('//books', function(req, res) {
+  
+app.post('/v1/books', function(req, res) {
   client.query(
-    `INSERT INTO books (title, author, isbn, url, description)
+    `INSERT INTO books (title, author, isbn, image_url, description)
     VALUES ($1, $2, $3, $4, $5);
     `,
     [
       req.body.title,
       req.body.author,
       req.body.isbn,
-      req.body.url,
+      req.body.image_url,
       req.body.description,
       
     ]
-  ) .then(function(data) {
-    res.send('insert complete');
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
+  )
+    .then(function(data) {
+      res.send('insert complete');
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
 });
 
-
-app.delete('/books/:id', (req, res) => {
-  client.query(`
-    DELETE FROM books
-    WHERE id=${req.params.id}
-    `)
-    .then(() => res.send('deleted'))
+// DELETE
+app.delete('/v1/books/:book_id', function(req, res) {
+  console.log(req.params.book_id);
+    client.query(`DELETE FROM books WHERE book_id=$1`,
+     [req.params.book_id]
+    )
+    .then(() => res.send('Delete complete'))
     .catch(console.error);
 });
 
-//Update a book
-app.put('/books/:id/edit', function(req, res) {
-   client.query(`UPDATE * FROM books WHERE id = ${req.params.id};`)
+// UPDATE/PUT
+app.put('/v1/books/:book_id/edit', function(req, res) {
+  console.log(req.body);
+  client.query(`UPDATE * FROM books WHERE book_id = ${req.params.book_id};`)
     .then(() => {
       client.query(`
       UPDATE books
-      SET title=$1, author=$2, isbn=$3, url=$4, description=$5
-      WHERE id = $6
+      SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5
+      WHERE book_id = $6
       `,
       [
         req.body.title,
         req.body.author,
         req.body.isbn,
-        req.body.url,
+        req.body.image_url,
         req.body.description,
-        req.params.id
+        req.params.book_id
       ]
       )
     })
-    .then(() => res.send('Update completed'))
+    .then(() => res.send('Update complete'))
     .catch(console.error);
 });
 
@@ -104,11 +119,11 @@ app.listen(PORT, () => {
 function createTable() {
   client.query(`
     CREATE TABLE IF NOT EXISTS books(
-      id SERIAL PRIMARY KEY,
+      book_id SERIAL PRIMARY KEY,
       title VARCHAR(255),
       author VARCHAR(255),
       isbn VARCHAR(255),
-      url VARCHAR(255),
+      image_url VARCHAR(255),
       description TEXT NOT NULL
     );`
   )      
