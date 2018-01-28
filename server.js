5fd909eeca7edede77ce3cfb5eda3636fc80bab6
andriiglukhyi <andrigluxii@gmail.com>
2018-01-27 16:25:41 -0800

    new

--- server.js
+++ server.js
@@ -18,52 +18,98 @@ client.connect();
 app.use(cors());
 
 
-app.get('/books', (req, res) => {
-  client.query(`SELECT * FROM books;`)
-    .then(results => res.send(results.rows))
-    .catch(console.error);
+app.get('/books', function(req, res) {
+  client.query('SELECT * FROM books;')
+  .then(function(data) {
+    res.send(data.rows);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
 });
 
-//Getting a single book
-app.get('/books/:id', (req, res) => {
-  client.query(`SELECT * FROM books WHERE id=${req.params.id}`)
-    .then(results => res.send(results.rows))
-    .catch(console.error);
+
+app.get('/books/:id', function (req,res) {
+  client.query(`SELECT * FROM books WHERE id = ${req.params.id};`)
+  .then(function(data){
+    res.send(data.rows);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 });
+
 
 //Adding a book to the database
-app.post('/books', bodyParser, (req, res) => {
-  let {title, author, isbn, url, description} = req.body;
-  client.query(`
-      INSERT INTO books(title, author, isbn, url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING`,
-    [title, author, isbn, url, description]
-  )
-    .then(res.sendStatus(201))
-    .catch(console.error);
+app.post('//books', function(req, res) {
+  client.query(
+    `INSERT INTO books (title, author, isbn, url, description)
+    VALUES ($1, $2, $3, $4, $5);
+    `,
+    [
+      req.body.title,
+      req.body.author,
+      req.body.isbn,
+      req.body.url,
+      req.body.description,
+      
+    ]
+  ) .then(function(data) {
+    res.send('insert complete');
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
 });
 
 
-//Delete a book
 app.delete('/books/:id', (req, res) => {
   client.query(`
     DELETE FROM books
-    WHERE id=${req.params.id}`)
-    .then(() => res.send(204))
+    WHERE id=${req.params.id}
+    `)
+    .then(() => res.send('deleted'))
     .catch(console.error);
 });
 
 //Update a book
-app.put('/books', bodyParser, (req, res) => {
-  let {title, author, isbn, url, description} = req.body;
-  client.query(`
-    UPDATE books
-    SET title=$1, author=$2, isbn=$3, url=$4, description=$5
-    WHERE id=${req.body.book_id}`,
-  [title, author, isbn, url, description]
-  )
-    .catch(console.error)
+app.put('/books/:id/edit', function(req, res) {
+   client.query(`UPDATE * FROM books WHERE id = ${req.params.id};`)
+    .then(() => {
+      client.query(`
+      UPDATE books
+      SET title=$1, author=$2, isbn=$3, url=$4, description=$5
+      WHERE id = $6
+      `,
+      [
+        req.body.title,
+        req.body.author,
+        req.body.isbn,
+        req.body.url,
+        req.body.description,
+        req.params.id
+      ]
+      )
+    })
+    .then(() => res.send('Update completed'))
+    .catch(console.error);
 });
 
-//This is a redirect
+createTable();
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+app.listen(PORT, () => {
+    console.log('SERVER started on port:', PORT);
+});
+
+function createTable() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS books(
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255),
+      author VARCHAR(255),
+      isbn VARCHAR(255),
+      url VARCHAR(255),
+      description TEXT NOT NULL
+    );`
+  )      
+};   
